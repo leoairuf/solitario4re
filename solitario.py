3c6219b105ced70ce6efd010f25f8fc77a1e0d4f
leoairuf <leonardo.furia@outlook.it>
2021-01-07 18:35:36 +0100

    aggiunte classi

--- solitario.py
+++ solitario.py
@@ -25,6 +25,7 @@ class Mazzo:
         for seme in semi:
             for valore in range(1, 11, 1):
                 self.lista_carte.append(Carta(seme,valore))
+                
         
     def mischia(self):
         random.shuffle(self.lista_carte)
@@ -39,9 +40,56 @@ class Mazzo:
         return prima_carta
     
      
-class TavoloDaGioco:
+class Strategia:
+    
     def __init__():
         pass
+    
+    def disponi_carte(mazzo,tavolo):
+        
+        for i in range(4):
+            riga=[]
+            for j in range(9):
+                carta=mazzo.prima_carta()
+                riga.append(carta)
+            tavolo.append(riga)
+    
+    def controlla_riga(carta):
+        pass
+        
+        
+        
+                
+            
+class Giocatore:
+    def __init__():
+        pass
+    
+    def vittoria():
+        #controlla ordinamento carte
+        pass
+    
+    def mazzetto_vuoto():
+        pass
+        
+        
+        
+class TavoloDaGioco:
+    def __init__(self,tavolo=[]):
+        self.tavolo=tavolo
+        
+    
+    def sostituisci_carta(self,mazzo,tavolo):
+        carta=mazzo.prima_carta()
+        riga=controlla_riga(carta)
+        tavolo[carta.valore-1][riga]
+        pass
+
+    
+    
+    
+    
+    
     
 
 mazzo=Mazzo()
@@ -56,18 +104,21 @@ mazzo.genera_mazzo_ordinato()
         
 mazzo.mischia()
 
+tavolo=TavoloDaGioco()
+Strategia.disponi_carte(mazzo,tavolo.tavolo)
+
 #check se sia stato mischiato
-for carta in mazzo.lista_carte:
+# for carta in mazzo.lista_carte:
   
-    print(carta.seme, carta.valore)
-    time.sleep(0.1)
+#     print(carta.seme, carta.valore)
+#     time.sleep(0.1)
 
-print('\n\n\n')
+# print('\n\n\n')
 
-while len(mazzo.lista_carte) != 0:
-    prima=mazzo.prima_carta()
-    print(prima.seme, prima.valore)
-    time.sleep(0.5)
+# while len(mazzo.lista_carte) != 0:
+#     prima=mazzo.prima_carta()
+#     print(prima.seme, prima.valore)
+#     time.sleep(0.5)
 
 
 
