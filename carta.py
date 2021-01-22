7ac16e409298ce7860741b0301df92b4457e60c0
leoairuf <leonardo.furia@outlook.it>
2021-01-22 17:09:26 +0100

    aggiunti moduli

---
+++ carta.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 14:58:21 2021
+
+@author: leona
+"""
+
+class Carta:
+    
+    
+    def __init__(self,seme=None,valore=None,coperta=True):
+        self.seme = seme
+        self.valore = valore
+        self.coperta = coperta
+     
+        
+    def is_carta(self,elem):
+        
+        '''Se la riga considerata ha la sintassi richiesta, viene creata una carta e restituito un valore logico True.
+           Altrimenti, viene segnalata con un valore logico False e ignorata.'''
+        
+        flag=False
+        if ',' in elem:
+            elem=elem.strip().split(',')
+            if len(elem)==2 and elem[1] in 'BSDC' and elem[0].isnumeric():
+                if int(elem[0]) > 0  and int(elem[0]) <= 10:
+                    self.seme=elem[1]
+                    self.valore=int(elem[0])
+                    flag=True
+                    
+        return flag
---
+++ strategia.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 15:11:43 2021
+
+@author: leona
+"""
+from abc import ABC, abstractmethod
+import random
+
+class Strategy(ABC):
+    
+    '''interface class'''
+    
+    @abstractmethod
+    def stabilisci_seme():
+        pass
+    
+    
+      
+    
+class SemePerRigaCasuale(Strategy):
+    
+    
+    def stabilisci_seme():
+        
+        '''Viene stabilita una corrispondenza casuale seme-riga per il posizionamento delle carte.'''
+        
+        semi = ['B', 'C', 'D', 'S']
+        indici = list(range(4))
+        righe = {}
+        
+        for seme in semi:
+            length = len(indici)
+            r = random.randrange(length)
+            righe[seme] = indici[r]
+            indici.pop(r)
+            
+        return righe 
+    
+    
+class SemePerRigaPrefissato(Strategy):
+    
+    def stabilisci_seme():
+        
+        '''Viene stabilita una corrispondenza fissa seme-riga per il posizionamento delle carte.'''
+        
+        semi = ['B', 'C', 'D', 'S']
+        righe = {}
+        
+        indice = 0
+        for seme in semi:
+            righe[seme] = indice
+            indice += 1
+            
+        return righe 
+
+
+class SemePerRigaDinamico(Strategy):
+    
+    '''Viene stabilita una corrispondenza seme-riga in modo dinamico'''
+    
+    def stabilisci_seme(cont,carta_in,righe):
+         
+        if carta_in.seme not in righe:
+            righe[carta_in.seme]=cont
+            cont+=1
+                                   
+        return righe,cont
+        
